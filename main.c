9c53603e0aee92f3d31aea0a9b68651807fb9cd2
Lachie King <kingy00112233@gmail.com>
2025-12-18 04:59:07 +0000

    create basic shell script that requires full path to call functions. add/update header file

--- main.c
+++ main.c
@@ -7,9 +7,54 @@
  *
  * Return: Always 0
  */
-int main(int argc, char **argv)
+int main(void)
 {
-(void)argc;
-shell_loop(argv);
-return (0);
+	char *line = NULL;
+	size_t len = 0;
+	int nchar;
+	pid_t child_pid;
+	int status;
+	char *argv[2];
+
+	while (1)
+	{
+		printf("($) ");
+		nchar = getline(&line, &len, stdin);
+		if (nchar == -1)
+		{
+			putchar('\n');
+			break;
+		}
+		
+		line[nchar - 1] = '\0';
+
+		if (line[0] == '\0')
+			continue;
+
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("fork");
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+
+		if (child_pid == 0)
+		{
+			argv[0] = line;
+			argv[1] = NULL;
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror("Error");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+
+	free(line);
+	return (0);
 }
---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void);
+
+#endif
