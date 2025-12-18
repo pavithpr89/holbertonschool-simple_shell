2d420ad3c6a04512d918b8bc1bc6bf60e647043c
pavithpr89 <pavithpr89@gmail.com>
2025-12-18 07:49:04 +0000

    Broke the main and made the execute file to handle child logic

--- shell.h
+++ shell.h
@@ -1,25 +1,10 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-
-#include <stddef.h> // added this
-
-
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <string.h>
+#include <stddef.h>
 
 extern char **environ;
 
 void execute_command(char *line);
 
 #endif /* SHELL_H */
-
-
-//int main(void);
-
-//#endif
