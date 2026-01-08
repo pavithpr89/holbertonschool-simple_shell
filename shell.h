48382820c7b145ef0abd21a2551124b56c2bb093
Lachie King <kingy00112233@gmail.com>
2026-01-08 01:51:29 +0000

    add comments to header file

--- shell.h
+++ shell.h
@@ -1,6 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/* standard library headers */
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,12 +9,19 @@
 #include <sys/wait.h>
 #include <string.h>
 
-
+/* access environment variables */
 extern char **environ;
 
+/* Executes a command line by forking and running the program */
 int execute_command(char *line);
+
+/* Resolves a command name to its full executable path */
 char *find_command(char *cmd);
+
+/* Retrieves the PATH environment variable from env array */
 char *get_path(char **env);
+
+/* Prints all current environment variables */
 void print_env(void);
 
 #endif /* SHELL_H */
