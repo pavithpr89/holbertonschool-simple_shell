60b2adc7a6446f42eb0ac862da0768f4df36536f
Lachie King <kingy00112233@gmail.com>
2026-01-08 01:39:15 +0000

    add comments to find_path

--- find_path.c
+++ find_path.c
@@ -10,9 +10,11 @@ char *find_command(char *cmd)
 	static char full_path[1024];
 	char *path, *path_copy, *dir;
 
+	/* check for NULL */
 	if (!cmd)
 		return (NULL);
 	
+	/* if command is already a path, check if it's executable */
 	if (strchr(cmd, '/'))
 	{
 		if (access(cmd, X_OK) == 0)
@@ -20,19 +22,25 @@ char *find_command(char *cmd)
 		return (NULL);
 	}
 
+	/* retrieve PATH environmentvariable */
 	path = get_path(environ);
 
 	if (!path || path[0] == '\0')
 		return (NULL);
 
+	/* Duplicate PATH to safely tokenize directories */
 	path_copy = strdup(path);
 	if (!path_copy)
 		return (NULL);
 
+	/* iterate through each directory in PATH */
 	dir = strtok(path_copy, ":" );
 	while (dir)
 	{
+		/* build full path */
 		sprintf(full_path, "%s/%s", dir, cmd);
+
+		/* check if command exists and is executable */
 		if(access(full_path, X_OK) == 0)
 		{
 			free(path_copy);
@@ -40,6 +48,7 @@ char *find_command(char *cmd)
 		}
 		dir = strtok(NULL, ":");
 	}
+	/* command not found */
 	free(path_copy);
 	return (NULL);
 }
